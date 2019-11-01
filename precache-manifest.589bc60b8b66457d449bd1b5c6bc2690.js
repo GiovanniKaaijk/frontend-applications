1d49b80685345f29b1e1c27bf0cfa256fd5404a7
GiovanniKaaijk <kaaijkg@gmail.com>
2019-11-01 09:38:56 +0100

    Updates

---
+++ precache-manifest.589bc60b8b66457d449bd1b5c6bc2690.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001e595f9cbd81d3368aa067ccd81467",
+    "url": "/frontend-applications/index.html"
+  },
+  {
+    "revision": "0e775f1596a9aa82f96c",
+    "url": "/frontend-applications/static/css/main.95ae2688.chunk.css"
+  },
+  {
+    "revision": "cf0503661a3e7db186ba",
+    "url": "/frontend-applications/static/js/2.05a1bd62.chunk.js"
+  },
+  {
+    "revision": "0e775f1596a9aa82f96c",
+    "url": "/frontend-applications/static/js/main.128e7bbb.chunk.js"
+  },
+  {
+    "revision": "e14b76fde58b0f82f633",
+    "url": "/frontend-applications/static/js/runtime-main.803219c5.js"
+  },
+  {
+    "revision": "6a618a659e19dd9c83383d7507f34746",
+    "url": "/frontend-applications/static/media/heart.6a618a65.svg"
+  },
+  {
+    "revision": "5a8cc0974e4188925eccd26bca02aa24",
+    "url": "/frontend-applications/static/media/pinpoint.5a8cc097.svg"
+  },
+  {
+    "revision": "e57e1c79bca21597cebc25a0229c4aea",
+    "url": "/frontend-applications/static/media/search-icon.e57e1c79.svg"
+  },
+  {
+    "revision": "77c1a552fd3d268bf9d62d220b704481",
+    "url": "/frontend-applications/static/media/wereldMuseum.77c1a552.png"
+  },
+  {
+    "revision": "5e5f535db47e7ef3e8e0627fd7a0643d",
+    "url": "/frontend-applications/static/media/whiteHeart.5e5f535d.svg"
+  },
+  {
+    "revision": "bc0e87562319008390985267f5f9332b",
+    "url": "/frontend-applications/static/media/worldmap.bc0e8756.jpeg"
+  }
+]);
